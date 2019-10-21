2d77cfd44fa7e79fbfff3a63183b8a86e9ccf1cb
Shakier009 <54508394+Shakier009@users.noreply.github.com>
2019-10-21 09:29:00 +0200

    Add files via upload

---
+++ src/main/java/za/ac/cput/service/BookingServiceImpl.java
@@ -0,0 +1,146 @@
+package za.ac.cput.service;
+
+import za.ac.cput.domain.Booking;
+import za.ac.cput.repository.BookingRepository;
+//import za.ac.cput.repository.impl.Airline.BookingRepositoryImpl;
+import za.ac.cput.service.Airline.BookingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import za.ac.cput.domain.Booking;
+import za.ac.cput.service.IService;
+import za.ac.cput.exception.RecordNotFoundException;
+import za.ac.cput.repository.BookingRepository;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.awt.print.Book;
+import java.util.List;
+import java.util.Set;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+public class BookingServiceImpl{
+
+
+    @Autowired
+
+    private BookingRepository bookingRepository;
+
+    private BookingServiceImpl (){}
+
+    public List<Booking> getAllBooking()
+    {
+        List<Booking> result = (List<Booking>) bookingRepository.findAll();
+
+        if(result.size() > 0) {
+            return result;
+        } else {
+            return new ArrayList<Booking>();
+        }
+    }
+
+    public Booking getBookingById(Long id) throws RecordNotFoundException
+    {
+        Optional<Booking> booking = bookingRepository.findById(id);
+
+        if(booking.isPresent()) {
+            return booking.get();
+        } else {
+            throw new RecordNotFoundException("No Booking exist for given invoive");
+        }
+    }
+
+    public Booking createOrUpdateBooking(Booking entity)
+    {
+        if(entity.getInvoice()  == null)
+        {
+            entity = bookingRepository.save(entity);
+
+            return entity;
+        }
+        else
+        {
+            Optional<Booking> booking = bookingRepository.findById(entity.getInvoice());
+
+            if(booking.isPresent())
+            {
+                Booking newEntity = booking.get();
+                newEntity.setDepTime(entity.getDepTime());
+                newEntity.setDestination(entity.getDestination());
+                newEntity.setInvoice(entity.getInvoice());
+                newEntity.setSeatNr(entity.getSeatNr());
+
+                newEntity = bookingRepository.save(newEntity);
+
+                return newEntity;
+            } else {
+                entity = bookingRepository.save(entity);
+
+                return entity;
+            }
+        }
+    }
+
+    public void deleteBookingById(Long id) throws RecordNotFoundException
+    {
+        Optional<Booking> booking = bookingRepository.findById(id);
+
+        if(booking.isPresent())
+        {
+            bookingRepository.deleteById(id);
+        } else {
+            throw new RecordNotFoundException("No Booking exist for given invoive");
+        }
+    }
+
+//    public static BookingServiceImpl getService(){
+//        if (service == null) service = new BookingServiceImpl();
+//        return service;
+//    }
+//
+//    @Override
+//    public Booking create(Booking booking) {
+//        return bookingRepository.save(booking);
+//    }
+//
+//    @Override
+//    public Booking update(Booking booking) {
+//        return bookingRepository.save(booking);
+//    }
+//
+//    @Override
+//    public void delete(String s) {
+//        bookingRepository.deleteById(s);
+//
+//    }
+//
+//    @Override
+//    public Booking read(String s) {
+//        Optional<Booking> optGender = this.bookingRepository.findById(s);
+//
+//        return optGender.orElse(null);
+//    }
+//
+//    @Override
+//    public Booking retrieveBy(String depte) {
+//        List<Booking> dept = geetAll();
+//        for (Booking dep : dept) {
+//            if (dep.getDepTime().equalsIgnoreCase(depte)) return dep;
+//        }
+//        return null;
+//    }
+//
+//    @Override
+//    public List<Booking> geetAll() {
+//        return (List<Booking>) bookingRepository.findAll();
+//    }
+
+}
