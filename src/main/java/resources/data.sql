51c7a9a330be71fb48f15cce03ed4c83c0448fd0
Shakier009 <54508394+Shakier009@users.noreply.github.com>
2019-10-21 09:26:07 +0200

    Add files via upload

---
+++ src/main/java/resources/data.sql
@@ -0,0 +1,36 @@
+--     INSERT INTO
+-- 	Booking_Info (departure_Time, destination, seatNr)
+-- VALUES
+--   	('09:22', 'Cape_Town', 'B5'),
+--   	('03:23', 'Gouteng', 'U7'),
+--   	('18:34','Durban', 'D3'),
+--     ('22:56','Limpopo', 'G9'),
+--     ('19:00','Durban', 'T8'),
+--     ('04:30','Soweto', 'C3'),
+--     ('06:15','Namibia', 'Z2');
+
+    INSERT INTO
+	TBL_EMPLOYEES (first_name, last_name, email)
+VALUES
+   	('09:22', 'Cape_Town', 'B5'),
+  	('03:23', 'Gouteng', 'U7'),
+  	('18:34','Durban', 'D3'),
+    ('22:56','Limpopo', 'G9'),
+    ('19:00','Durban', 'T8'),
+    ('04:30','Soweto', 'C3'),
+    ('06:15','Namibia', 'Z2');
+
+--     INSERT INTO
+-- 	TBL_EMPLOYEES (first_name, last_name, email)
+-- VALUES
+--   	('James', 'May', 'jmay@gmail.com'),
+--   	('Richard', 'Hammond', 'rhammond@gmail.com'),
+--   	('Jeremy','Clarkson','jclarkson@gmail.com'),
+--     ('Christian','Meadows','cmeadows@gmail.com'),
+--     ('Nicole','Rollins','nrollins@gmail.com'),
+--     ('Ashton','Chambers','achambers@gmail.com'),
+--     ('Carol','Glazier','cglazier@gmail.com');
+
+
+
+
